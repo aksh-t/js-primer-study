838ada2ce4eaa152da364d38c5a366762f07b712
aksh-t <tanno.ao@gmail.com>
2020-05-12 01:44:02 +0900

    浮動小数点数リテラルと文字列リテラル

--- basic/data-type/index.js
+++ basic/data-type/index.js
@@ -47,7 +47,49 @@ console.log(0xFF);
 console.log(0xff);
 console.log(0x30A2);
 
+// ## 浮動小数点数リテラル
+console.log(.123);
+console.log(2e8);
 
+// # 文字列（String）
+console.log("文字列");
+console.log('文字列');
+console.log(`文字列`);
+
+console.log('8 o\'clock');
+console.log("8 o'clock");
+
+// # ダブルクォートとシングルクォートでは改行を含んだ文字列は定義できない
+// console.log("複数行の
+// 文字列を
+// 入れたい");
+
+console.log("複数行の\n文字列を\n入れたい");
+
+// ## [ES2015] テンプレートリテラル
+console.log(`複数行の
+文字列を
+入れたい`);
+
+const str2 = "文字列";
+console.log(`これは${str2}です`);
+
+console.log(`This is \`code\``);
+
+// # nullリテラル
+const foo = null;
+console.log(foo);
+
+// # undefinedはリテラルではない
+// ## undefinedはただのグローバル変数で、undefinedという値を持っているだけ
+function fn(){
+    var undefined = "独自の未定義値"; // undefinedという名前の変数をエラーなく定義できる
+    console.log(undefined); // => "独自の未定義値"
+}
+fn();
+
+// # true、false、nullなどはグローバル変数ではなくリテラル
+// var null; // => SyntaxError
 
 
 // また、オブジェクトの中でもよく利用されるものに関してはリテラル表現が用意されている
