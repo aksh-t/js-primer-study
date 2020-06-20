dd9b8fc8ea52c1ef48dbefacdd39fa639b1c606c
aksh-t <tanno.ao@gmail.com>
2020-06-21 01:25:28 +0900

    WIP: 条件分岐

---
+++ basic/condition/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+{
+    if (true) {
+        console.log("この行は実行されます")
+    }
+}
+
+{
+    if (true)
+        console.log("この行は実行されます")
+}
+
+{
+    const x = 42
+    if (x > 10) {
+        console.log("xは10より大きな値です")
+    }
+}
