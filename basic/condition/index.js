d4f8d47ba91a97c6a8f0e117203ef70202be5de0
aksh-t <tanno.ao@gmail.com>
2020-06-24 00:31:14 +0900

    条件分岐

--- basic/condition/index.js
+++ basic/condition/index.js
@@ -103,3 +103,64 @@
         console.log(`${year}はうるう年ではありません`)
     }
 }
+
+{
+    const version = "ES6"
+    switch (version) {
+        case "ES5":
+            console.log("ECMAScript 5")
+            break
+        case "ES6":
+            console.log("ECMAScript 2015")
+            break
+        case "ES7":
+            console.log("ECMAScript 2016")
+            break
+        default:
+            console.log("しらないバージョンです")
+            break
+    }
+}
+
+{
+    const version = "ES6"
+    if (version === "ES5") {
+        console.log("ECMAScript 5")
+    } else if (version === "ES6") {
+        console.log("ECMAScript 2015")
+    } else if (version === "ES7") {
+        console.log("ECMAScript 2016")
+    } else {
+        console.log("しらないバージョンです")
+    }
+}
+
+{
+    const version = "ES6"
+    switch (version) {
+        case "ES5":
+            console.log("ECMAScript 5")
+        case "ES6":
+            console.log("ECMAScript 2015")
+        case "ES7":
+            console.log("ECMAScript 2016")
+        default:
+            console.log("しらないバージョンです")
+    }
+}
+
+{
+    function getECMAScriptName(version) {
+        switch (version) {
+            case "ES5":
+                return "ECMAScript 5"
+            case "ES6":
+                return "ECMAScript 2015"
+            case "ES7":
+                return "ECMAScript 2016"
+            default:
+                return "しらないバージョンです"
+        }
+    }
+    console.log(getECMAScriptName("ES6"))
+}
