24ff8171cc8b23b88b4e090cb234d2df837057b8
aksh-t <tanno.ao@gmail.com>
2020-06-25 23:40:17 +0900

    WIP: ループと反復処理

--- basic/loop/index.js
+++ basic/loop/index.js
@@ -2,10 +2,18 @@
 
 {
     let x = 0;
-    console.log(`ループ開始前のxの値: ${x}`)
+    console.log(`ループ開始前のxの値: ${x}`);
     while (x < 10) {
-        console.log(x)
-        x += 1
+        console.log(x);
+        x += 1;
     }
-    console.log(`ループ終了後のxの値: ${x}`)
+    console.log(`ループ終了後のxの値: ${x}`);
+}
+
+{
+    // let i = 1;
+    // while (i > 0) {
+    //     console.log(`${i}回目のループ`);
+    //     i += 1;
+    // }
 }
