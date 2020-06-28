9dc26213e8ad701766163892f9dac00c5254a1ba
aksh-t <tanno.ao@gmail.com>
2020-06-29 02:13:52 +0900

    WIP: オブジェクト

---
+++ basic/object/index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+{
+    const ob = {};
+}
+
+{
+    const obj = {
+        "key": "value"
+    };
+}
+
+{
+    const obj = {
+        key: "value"
+    };
+}
+
+{
+    //     const object = {
+    //         my-prop: "value";
+    //     }
+    {
+        const obj = {
+            "my-prop": "value"
+        };
+    }
+}
+
+{
+    const color = {
+        red: "red",
+        green: "green",
+        blue: "blue"
+    };
+}
+
+{
+    const name = "名前";
+    const obj = {
+        name: name
+    };
+    console.log(obj);
+}
+
+{
+    const name = "名前";
+    const obj = {
+        name
+    };
+    console.log(obj);
+}
