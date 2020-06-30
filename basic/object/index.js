05016ad32c2bcca6f3b1887ab31ce81c904c3887
aksh-t <tanno.ao@gmail.com>
2020-07-01 01:30:56 +0900

    WIP: オブジェクト

--- basic/object/index.js
+++ basic/object/index.js
@@ -55,3 +55,34 @@
     const obj = new Object();
     console.log(obj);
 }
+
+{
+    const obj = {
+        key: "value"
+    };
+    // ドット記法
+    console.log(obj.key);
+    // ブラケット記法
+    console.log(obj["key"]);
+}
+
+{
+    const obj = {
+        key: "value",
+        123: 456,
+        "my-key": "my-value"
+    };
+
+    console.log(obj["key"]);
+    console.log(obj[123]);
+    console.log(obj["my-key"]);
+}
+
+{
+    const languages = {
+        ja: "日本語",
+        en: "英語"
+    };
+    const myLang = "ja";
+    console.log(languages[myLang]);
+}
