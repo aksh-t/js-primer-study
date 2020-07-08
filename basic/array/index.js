d8e3f4b975f46d7af8452f20c3fbb8fc54057724
aksh-t <tanno.ao@gmail.com>
2020-07-08 22:46:20 +0900

    配列

--- basic/array/index.js
+++ basic/array/index.js
@@ -49,3 +49,48 @@
     console.log(sparseArray.length);
     console.log(sparseArray[1]);
 }
+
+{
+    const obj = {};
+    const array = [];
+    console.log(Array.isArray(obj));
+    console.log(Array.isArray(array));
+}
+
+{
+    const array = [];
+    // "object" が返る
+    console.log(typeof array);
+}
+
+{
+    // ES2015
+    const typedArray = new Int8Array(8);
+    console.log(Array.isArray(typedArray));
+}
+
+{
+    // ES2015
+    // 分割代入(Destructuring assignment)
+    const array = ["one", "two", "three"];
+    const [first, second, third] = array;
+    console.log(first);
+    console.log(second);
+    console.log(third);
+}
+
+{
+    const denseArray = [1, undefined, 3];
+    const sparseArray = [1, , 3];
+    // 判断できない
+    console.log(denseArray[1]);
+    console.log(sparseArray[1]);
+}
+
+{
+    const denseArray = [1, undefined, 3];
+    const sparseArray = [1, , 3];
+    // 判断できる
+    console.log(denseArray.hasOwnProperty(1));
+    console.log(sparseArray.hasOwnProperty(1));
+}
