b4b15159c0a3ef469dcd00697c7f5ed11c0666db
aksh-t <tanno.ao@gmail.com>
2020-07-11 02:25:43 +0900

    条件に一致する要素を取得

--- basic/array/index.js
+++ basic/array/index.js
@@ -128,3 +128,21 @@
     console.log(indexOfBlue);
     console.log(colors[indexOfBlue]);
 }
+
+{
+    // ES2015
+    const colors = [
+        { "color": "red" },
+        { "color": "green" },
+        { "color": "blue" }
+    ];
+    const blueColor = colors.find((obj) => {
+        return obj.color === "blue";
+    });
+    console.log(blueColor);
+
+    const whiteColor = colors.find((obj) => {
+        return obj.color === "white";
+    });
+    console.log(whiteColor);
+}
