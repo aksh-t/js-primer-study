9542998d8123922e7f42feab34f3a523be88639a
aksh-t <tanno.ao@gmail.com>
2020-07-24 02:00:21 +0900

    Code Point

--- basic/string-unicode/index.js
+++ basic/string-unicode/index.js
@@ -1,1 +1,16 @@
 "use strict";
+{
+    console.log("あ".codePointAt(0));
+}
+
+{
+    console.log(String.fromCodePoint(12354));
+    console.log(String.fromCodePoint(0x3042));
+}
+
+{
+    const codePointOfあ = "あ".codePointAt(0);
+    const hexOfあ = codePointOfあ.toString(16);
+    console.log(hexOfあ);
+    console.log("\u{3042}");
+}
