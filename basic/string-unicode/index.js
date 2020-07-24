7b8fd3fbf40080860a8ff7335daa66322bd2ad44
aksh-t <tanno.ao@gmail.com>
2020-07-25 00:51:20 +0900

    Code PointとCode Unitの違い

--- basic/string-unicode/index.js
+++ basic/string-unicode/index.js
@@ -14,3 +14,35 @@
     console.log(hexOfあ);
     console.log("\u{3042}");
 }
+
+{
+    function convertCodeUnits(str) {
+        const codeUnits = [];
+        for (let i = 0; i < str.length; i++) {
+            codeUnits.push(str.charCodeAt(i).toString(16));
+        }
+        return codeUnits;
+    }
+
+    function convertCodePoints(str) {
+        return Array.from(str).map(char => {
+            return char.codePointAt(0).toString(16);
+        });
+    }
+
+    {
+        const str = "アオイ";
+        const codeUnits = convertCodeUnits(str);
+        console.log(codeUnits);
+        const codePoints = convertCodePoints(str);
+        console.log(codePoints);
+    }
+
+    {
+        const str = "リンゴ🍎";
+        const codeUnits = convertCodeUnits(str);
+        console.log(codeUnits);
+        const codePoints = convertCodePoints(str);
+        console.log(codePoints);
+    }
+}
