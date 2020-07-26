a82674d57e20b5b56b3cb04fe33c65a92d74c510
aksh-t <tanno.ao@gmail.com>
2020-07-26 14:21:09 +0900

    プリミティブ型とラッパーオブジェクト

--- basic/wrapper-object/index.js
+++ basic/wrapper-object/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+{
+    const str = new String("input value");
+    console.log(str.toUpperCase());
+}
+
+{
+    const str = "文字列";
+    console.log(typeof str);
+
+    const stringWrapper = new String("文字列");
+    console.log(typeof stringWrapper)
+}
