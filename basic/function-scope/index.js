7a73a73f56225be37250f612da3d02b5dae0ad39
aksh-t <tanno.ao@gmail.com>
2020-07-28 00:06:38 +0900

    スコープとは

--- basic/function-scope/index.js
+++ basic/function-scope/index.js
@@ -1,1 +1,33 @@
 "use strict";
+
+{
+    function fn() {
+        const x = 1;
+        console.log(x);
+    }
+    fn();
+    // console.log(x);
+}
+
+{
+    function fn(arg) {
+        console.log(arg);
+    }
+    fn(1);
+    // console.log(arg);
+}
+
+{
+    let a;
+    // let a;
+}
+
+{
+    function fnA() {
+        let x;
+    }
+
+    function fnB() {
+        let x;
+    }
+}
