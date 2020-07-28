696037aa7431b6ec6cf386f378ba0354aa2b9e68
aksh-t <tanno.ao@gmail.com>
2020-07-29 00:15:03 +0900

    静的スコープ

--- basic/function-scope/index.js
+++ basic/function-scope/index.js
@@ -243,3 +243,19 @@ console.log(Array);
     console.log(myCounter());
     console.log(newCounter());
 }
+
+{
+    // 静的スコープ
+    const x = 10;
+
+    function printX() {
+        console.log(x);
+    }
+
+    function run() {
+        const x = 20;
+        printX();
+    }
+
+    run();
+}
