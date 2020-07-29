e9593a6fd6f91ef16ce412ccb6ab036c0660530e
aksh-t <tanno.ao@gmail.com>
2020-07-29 23:38:15 +0900

    関数とメソッドにおけるthis

--- basic/function-this/index.js
+++ basic/function-this/index.js
@@ -1,1 +1,30 @@
 "use strict";
+// 関数の種類
+{
+    // `function`キーワードによる関数宣言
+    function fn1() { }
+    // `function`を式として扱う関数式
+    const fn2 = function () { };
+    // Arrow Functionを使った関数式
+    const fn3 = () => { };
+}
+
+// メソッドの種類
+{
+    // JavaScriptではオブジェクトのプロパティが関数である場合に、それをメソッドと呼ぶ
+    const obj = {
+        method1: function () {
+        },
+        method2: () => {
+        }
+    };
+}
+
+{
+    const obj = {
+        // メソッドの短縮記法
+        method() {
+        }
+    };
+}
+
