37821a4818e3f2dac088c9360a117f6dee39d1ee
aksh-t <tanno.ao@gmail.com>
2020-07-29 23:39:00 +0900

    Arrow Function以外の関数におけるthis

--- basic/function-this/index.js
+++ basic/function-this/index.js
@@ -28,3 +28,95 @@
     };
 }
 
+// Arrow Function以外の関数におけるthis
+{
+    function fn() { }
+    const obj = {
+        method() { }
+    };
+    const obj1 = {
+        obj2: {
+            method() { }
+        }
+    };
+    // thisの基本的な参照先はベースオブジェクト
+    // undefined
+    fn();
+    // obj
+    obj.method();
+    // obj2
+    obj1.obj2.method();
+    // obj2
+    obj1["obj2"]["method"]();
+}
+
+// 関数宣言や関数式におけるthis
+{
+    "use strict";
+    function fn1() {
+        return this;
+    }
+    const fn2 = function () {
+        return this;
+    };
+    console.log(fn1());
+    console.log(fn2());
+}
+
+{
+    "use strict";
+    function outer() {
+        console.log(this);
+        function inner() {
+            console.log(this);
+        }
+        inner();
+    }
+    outer();
+}
+
+// strict modeではない場合
+// thisがundefinedだとグローバルオブジェクトへの参照に変換される
+
+// strict modeの場合
+// メソッド以外の関数におけるthisはundefinedになるので、使う必要はない
+
+
+// メソッド呼び出しにおけるthis
+{
+    const obj = {
+        method1: function () {
+            return this;
+        },
+        method2() {
+            return this;
+        }
+    };
+    console.log(obj.method1());
+    console.log(obj.method2());
+}
+
+{
+    const person = {
+        fullName: "Brendan Eich",
+        sayName: function () {
+            // 同じオブジェクトの別プロパティを参照できる
+            return this.fullName;
+        }
+    };
+    console.log(person.sayName());
+}
+
+{
+    const obj1 = {
+        obj2: {
+            obj3: {
+                method() {
+                    return this;
+                }
+            }
+        }
+    };
+    console.log(obj1.obj2.obj3.method() === obj1.obj2.obj3);
+}
+
