59756b5ceb0c84de478c018ab97e5484ebaa163a
aksh-t <tanno.ao@gmail.com>
2020-08-01 01:07:49 +0900

    クラスの定義

--- basic/class/index.js
+++ basic/class/index.js
@@ -1,1 +1,32 @@
 "use strict";
+{
+    // クラス宣言文
+    class MyClass {
+        constructor() {
+            // 
+        }
+    }
+}
+
+{
+    // クラス式
+    const MyClass = class MyClass {
+        constructor() { }
+    };
+
+    const AnonymousClass = class {
+        constructor() { }
+    };
+}
+
+{
+    class MyClassA {
+        constructor() {
+
+        }
+    }
+
+    class MyClassB {
+
+    }
+}
