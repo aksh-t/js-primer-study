4859dba7b09ff6e525f06dd1596c5a7ce5ee437e
aksh-t <tanno.ao@gmail.com>
2020-08-01 01:15:56 +0900

    クラスのインスタンス化

--- basic/class/index.js
+++ basic/class/index.js
@@ -30,3 +30,48 @@
 
     }
 }
+
+{
+    class MyClass {
+    }
+
+    const myClass = new MyClass();
+    const myClassAnother = new MyClass();
+    console.log(myClass === myClassAnother);
+    console.log(myClass instanceof MyClass);
+    console.log(myClassAnother instanceof MyClass);
+}
+
+{
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    const point = new Point(3, 4);
+    console.log(point.x);
+    console.log(point.y);
+}
+
+{
+    class MyClass {
+        constructor() { }
+    }
+
+    // Uncaught TypeError: Class constructor MyClass cannot be invoked without 'new'
+    // MyClass();
+}
+
+{
+    class Point {
+        constructor(x, y) {
+            return { x, y };
+        }
+    }
+
+    const point = new Point();
+    console.log(point);
+    console.log(point instanceof Point);
+}
