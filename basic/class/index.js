c0cebdf544ba967c2206b97a3849426dc0335026
aksh-t <tanno.ao@gmail.com>
2020-08-02 22:08:36 +0900

    クラスのアクセッサプロパティの定義

--- basic/class/index.js
+++ basic/class/index.js
@@ -190,3 +190,26 @@
     const method = instance.method;
     console.log(method());
 }
+
+{
+    // アクセッサプロパティ
+    class NumberWrapper {
+        constructor(value) {
+            this._value = value;
+        }
+        get value() {
+            console.log("getter");
+            return this._value;
+        }
+        set value(newValue) {
+            console.log("setter");
+            this._value = newValue;
+        }
+    }
+
+    const numberWrapper = new NumberWrapper(1);
+    console.log(numberWrapper.value);
+
+    numberWrapper.value = 42;
+    console.log(numberWrapper.value);
+}
