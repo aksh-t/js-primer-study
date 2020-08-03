8b5ab83c25162b393717f8b2d11db4913d6e9cd5
aksh-t <tanno.ao@gmail.com>
2020-08-04 01:30:43 +0900

    ビルトインオブジェクトの継承

--- basic/class/index.js
+++ basic/class/index.js
@@ -525,3 +525,28 @@
     console.log(child instanceof Parent);
     console.log(child instanceof Child);
 }
+
+{
+    class MyArray extends Array {
+        get first() {
+            if (this.length === 0) {
+                return undefined;
+            } else {
+                return this[0];
+            }
+        }
+
+        get last() {
+            if (this.length === 0) {
+                return undefined;
+            } else {
+                return this[this.length - 1];
+            }
+        }
+    }
+
+    const array = MyArray.from([1, 2, 3, 4, 5]);
+    console.log(array.length);
+    console.log(array.first);
+    console.log(array.last);
+}
