27a0f91420eb406b0e206716b471a4eb4f6e0882
aksh-t <tanno.ao@gmail.com>
2020-08-04 23:04:00 +0900

    try...catch構文

--- basic/error-try-catch/index.js
+++ basic/error-try-catch/index.js
@@ -1,1 +1,29 @@
 "use strict";
+{
+    try {
+        console.log("try節:この行は実行されます");
+        undefinedFunction();
+    } catch (error) {
+        console.log("catch節:この行は実行されます");
+        console.log(error instanceof ReferenceError);
+        console.log(error.message);
+    } finally {
+        console.log("finally節:この行は実行されます");
+    }
+}
+
+try {
+    try {
+        undefinedFunction();
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        undefinedFunction();
+    } finally {
+        console.log("この行は実行されます");
+    }
+} catch (e) {
+}
+
