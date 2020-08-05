6b05a273317266af42b3871efcd2c25f094a5bdb
aksh-t <tanno.ao@gmail.com>
2020-08-06 01:04:46 +0900

    同期処理

--- basic/async/index.js
+++ basic/async/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// 同期処理
+{
+    function blockTime(timeout) {
+        const startTime = Date.now();
+        while (true) {
+            const diffTime = Date.now() - startTime;
+            if (diffTime >= timeout) {
+                return;
+            }
+        }
+    }
+    console.log("処理を開始");
+    blockTime(1000);
+    console.log("この行が呼ばれるまで処理が1秒間ブロックされる");
+}
+// 同期的にブロックする処理があると、ブラウザのメインスレッドがブロックされる
