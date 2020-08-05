906d9dcab26ef85add2f183ebf5b8d5999d5787a
aksh-t <tanno.ao@gmail.com>
2020-08-06 01:42:41 +0900

    エラーファーストコールバック

--- basic/async/index.js
+++ basic/async/index.js
@@ -84,3 +84,76 @@
     console.log("この行は実行されます");
     // このやり方だと非同期処理の外からは非同期処理の中で例外が発生したかがわからない
 }
+
+// エラーファーストコールバック
+try {
+    fs.readFile("./example.txt", (error, data) => {
+        if (error) {
+            // 読み込み中にエラーが発生しました
+        } else {
+            // データを読み込むことができました
+        }
+    });
+} catch (e) {
+    // ↑はNode.jsのAPIをコールしていてブラウザで実行すると例外が出るので、握り潰す
+}
+
+{
+    /**
+     * 1000ミリ秒未満のランダムなタイミングでレスポンスを擬似的にデータ取得する関数
+     * 指定した`path`にデータがある場合は`callback(null, レスポンス)`を呼ぶ
+     * 指定した`path`にデータがない場合は`callback(エラー)`を呼ぶ
+     */
+    function dummyFetch(path, callback) {
+        setTimeout(() => {
+            if (path.startsWith("/success")) {
+                callback(null, { body: `Response body of ${path}` });
+            } else {
+                callback(new Error("NOT FOUND"));
+            }
+        }, 1000 * Math.random());
+    }
+
+    dummyFetch("/success/data", (error, response) => {
+        if (error) {
+            // この行は実行されません
+        } else {
+            console.log(response);
+        }
+    });
+
+    dummyFetch("failure/data", (error, response) => {
+        if (error) {
+            console.log(error.message);
+        } else {
+            // この行が実行されません
+        }
+    });
+}
+
+{
+    // 成功用と失敗用の2つのコールバックを受け取るスタイル
+    /**
+     * リソースの取得に成功した場合は`successCallback(レスポンス)`を呼び出す
+     * リソースの取得に失敗した場合は`failureCallback(エラー)`を呼び出す
+     */
+    function dummyFetch(path, successCallback, failureCallback) {
+        setTimeout(() => {
+            if (path.startsWith("/success")) {
+                successCallback({ body: `Response body of ${path}` });
+            } else {
+                failureCallback(new Error("NOT FOUND"));
+            }
+        }, 1000 * Math.random());
+    }
+
+    dummyFetch("/success/data",
+        (response) => { console.log(response); },
+        (error) => { console.log(error.message); }
+    );
+
+    dummyFetch("failure/data",
+        (response) => { console.log(response); },
+        (error) => { console.log(error.message); }
+    );
+}
