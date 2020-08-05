021ff7a4574552039881928f9612e366232b2a09
aksh-t <tanno.ao@gmail.com>
2020-08-06 01:08:09 +0900

    非同期処理

--- basic/async/index.js
+++ basic/async/index.js
@@ -16,3 +16,24 @@
     console.log("この行が呼ばれるまで処理が1秒間ブロックされる");
 }
 // 同期的にブロックする処理があると、ブラウザのメインスレッドがブロックされる
+
+// 非同期処理
+{
+    function blockTime(timeout) {
+        const startTime = Date.now();
+        while (true) {
+            const diffTime = Date.now();
+            if (diffTime >= timeout) {
+                return;
+            }
+        }
+    }
+
+    console.log("1. setTimeoutのコールバック関数を10ミリ秒後に実行します");
+    setTimeout(() => {
+        console.log("3. ブロックする処理を開始します");
+        blockTime(1000);
+        console.log("4. ブロックする処理が完了しました");
+    }, 10);
+    console.log("2. 同期的な処理を実行します");
+}
