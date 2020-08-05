bf13964848e9505797399fe291a1de19c67f16ca
aksh-t <tanno.ao@gmail.com>
2020-08-06 01:16:15 +0900

    非同期処理はメインスレッドで実行される

--- basic/async/index.js
+++ basic/async/index.js
@@ -37,3 +37,25 @@
     }, 10);
     console.log("2. 同期的な処理を実行します");
 }
+
+// 非同期処理はメインスレッドで実行される
+{
+    function blockTime(timeout) {
+        const startTime = Date.now();
+        while (true) {
+            const diffTime = Date.now() - startTime;
+            if (diffTime >= timeout) {
+                return;
+            }
+        }
+    }
+
+    const startTime = Date.now();
+    setTimeout(() => {
+        const endTime = Date.now();
+        console.log(`非同期処理のコールバックが呼ばれるまで${endTime - startTime}ミリ秒かかりました`);
+    }, 10);
+    console.log("ブロックする処理を開始します");
+    blockTime(1000);
+    console.log("ブロックする処理が完了しました");
+}
