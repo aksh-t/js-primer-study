0192b0b9e732304f5fc4595db19b310471510059
aksh-t <tanno.ao@gmail.com>
2020-08-18 00:35:04 +0900

    HTTP通信

--- use-case/ajaxapp/index.js
+++ use-case/ajaxapp/index.js
@@ -1,8 +1,32 @@
-console.log("index.js: loaded");
+function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status);
+            if (!response.ok) {
+                console.error("エラーレスポンス", response);
+            } else {
+                return response.json().then(userInfo => {
+                    console.log(userInfo);
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
 
-const heading = document.querySelector("h2");
-const headingText = heading.textContent;
-
-const button = document.createElement("button");
-button.textContent = "Push Me";
-document.body.appendChild(button);
+function fetchUserInfoUsingXHR(userId) {
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://api.github.com/users/${encodeURIComponent(userId)}`);
+    request.addEventListener("load", () => {
+        if (request.status >= 200 && request.status < 300) {
+            const userInfo = JSON.parse(request.responseText);
+            console.log(userInfo);
+        } else {
+            console.error("エラーレスポンス", request.statusText);
+        }
+    });
+    request.addEventListener("error", () => {
+        console.error("ネットワークエラー");
+    });
+    request.send();
+}
