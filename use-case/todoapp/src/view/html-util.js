1660ce848ecd68755d2da8cf3670cb35e8bc215e
aksh-t <tanno.ao@gmail.com>
2020-08-21 01:37:50 +0900

    fix: typo

--- use-case/todoapp/src/view/html-util.js
+++ use-case/todoapp/src/view/html-util.js
@@ -19,13 +19,13 @@ export function htmlToElement(html) {
 
 /**
  * HTML文字列からDOM Nodeを作成して返すタグ関数
- * @return  {Element}
+ * @return {Element}
  */
 export function element(strings, ...values) {
     const htmlString = strings.reduce((result, str, i) => {
         const value = values[i - 1];
         if (typeof value === "string") {
-            return result = escapeSpecialChars(value) + str;
+            return result + escapeSpecialChars(value) + str;
         } else {
             return result + String(value) + str;
         }
@@ -33,7 +33,14 @@ export function element(strings, ...values) {
     return htmlToElement(htmlString);
 }
 
+/**
+ * コンテナ要素の中身をbodyElementで上書きする
+ * @param {Element} bodyElement コンテナ要素の中身となる要素
+ * @param {Element} containerElement コンテナ要素
+ */
 export function render(bodyElement, containerElement) {
+    // containerElementの中身を空にする
     containerElement.innerHTML = "";
+    // containerElementの直下にbodyElementを追加する
     containerElement.appendChild(bodyElement);
 }
